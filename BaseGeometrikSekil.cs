bc5e0a60fe1ed5f0560a84128f633e4ed339b17e
merve-celik16 <mervedursunn34@gmail.com>
2024-10-28 00:42:33 +0300

    Proje dosyası ekle.

---
+++ BaseGeometrikSekil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+
+        
+    }
+    public class Kare : BaseGeometrikSekil
+    {
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class Ucgen : BaseGeometrikSekil
+    {
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+
+    }
+
+    }
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           BaseGeometrikSekil kare = new Kare { Genislik = 10, Yukseklik = 5 };
+            BaseGeometrikSekil dikdortgen = new Dikdortgen { Genislik = 4, Yukseklik = 6 };
+            Ucgen ucgen = new Ucgen { Genislik = 5, Yukseklik = 3 };
+
+            Console.WriteLine($"Karenin Alanı: {kare.AlanHesapla()}");
+            Console.WriteLine($"Dikdörtgenin Alanı: {dikdortgen.AlanHesapla()}");
+            Console.WriteLine($"Üçgenin Alanı: {ucgen.AlanHesapla()}");
+        }
+    }
+}
